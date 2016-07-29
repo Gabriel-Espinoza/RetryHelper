ae694c146f0014f1aa7d7627ef11da5c7ead1576
Gabriel Espinoza <gab.espinoza@gmail.com>
2016-07-29 16:08:50 -0400

    Initial Commit of Gabo.RetryHelper

---
+++ Gabo.RetryHelper/Gabo.RetryHelper.Tests/Gabo.RetryHelper.Try.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gabo.RetryHelper.Tests
+{
+    [TestClass]
+    public class TestsTry
+    {
+        [TestMethod]
+        public void TestSleepTime()
+        {
+            string exceptionMessage = "Test Exception";
+            Stopwatch sw = new Stopwatch();
+            int millisecondsToWait = 50;
+            int maxTries = 7;
+
+            int count = 0;
+            try
+            {
+                sw.Start();
+                RetryHelper.Try(() =>
+                {
+                    count++;
+                    throw new Exception(exceptionMessage);
+                }, maxTries, millisecondsToWait);
+            }
+            catch (Exception ex)
+            {
+                sw.Stop();
+                Debug.Assert(count == maxTries, "The ammount of tries was not the intended");
+
+                if (ex.Message.Equals(exceptionMessage))
+                {
+                    var elapsed = sw.ElapsedMilliseconds;
+                    var minimumTime = millisecondsToWait*(maxTries-1);
+
+                    Debug.Assert(elapsed>minimumTime,
+                        "RetryHelper was not able to handle waiting time, it lasted less time than supposed to.","{0} > {1}", elapsed, minimumTime);
+                }
+                else
+                {
+                    Debug.Fail("Exception thrown by RetryHelper was not the one happening inside the Action.");
+                }
+                return;
+            }
+            Debug.Fail("RetryHelper was not able to catch the exception and hence this message was thrown");
+        }
+
+        [TestMethod]
+        public void TestMaxRetries()
+        {
+            string exceptionMessage = "Test Exception";
+            int millisecondsToWait = 50;
+            int maxTries = 7;
+
+            int count = 0;
+            try
+            {
+                RetryHelper.Try(() =>
+                {
+                    count++;
+                    throw new Exception(exceptionMessage);
+                }, maxTries, millisecondsToWait);
+            }
+            catch (Exception ex)
+            {
+                Debug.Assert(count == maxTries, "The ammount of tries was not the intended");
+                return;
+            }
+            Debug.Fail("RetryHelper was not able to catch the exception and hence this message was thrown");
+        }
+
+        [TestMethod]
+        public void TestNoException()
+        {
+            var a = 1;
+            RetryHelper.Try(() =>
+            {
+                a++;
+            });
+
+            Debug.Assert(a == 2, "The invoked method was not executed propperly");
+
+        }
+
+    }
+}
---
+++ Gabo.RetryHelper/Gabo.RetryHelper/RetryHelper.cs
@@ -0,0 +1,42 @@
+﻿namespace Gabo.RetryHelper
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// A simple class to retry actions until no exceptions are thrown
+    /// </summary>
+    public class RetryHelper
+    {
+        /// <summary>
+        /// Retry an action until no exceptions are thrown. After all tries, if exceptions still hapen, it will throw the 
+        /// original exception to the caller.
+        /// </summary>
+        /// <param name="actionToInvoke">Action to execute</param>
+        /// <param name="maxTries">Number of max tries before throwing the exception (default 5)</param>
+        /// <param name="sleepMilliseconds">Milliseconds to wait on the thread before trying again</param>
+        public static void Try(Action actionToInvoke, int maxTries = 5, int sleepMilliseconds = 100)
+        {
+            int errorCount = 0;
+            while (errorCount < maxTries)
+            {
+                try
+                {
+                    actionToInvoke.Invoke();
+                    return;
+                }
+                catch
+                {
+                    errorCount++;
+                    if (errorCount < maxTries)
+                    {
+                        Thread.Sleep(sleepMilliseconds);
+                        continue;
+                    }
+
+                    throw;
+                }
+            }
+        }
+    }
+}
